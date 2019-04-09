771cae6733792bc17bb705c4fafc5f1b7da357ff
Seth Dickerson <Seth.Dickerson@Ferguson.com>
2019-04-09 13:47:51 -0400

    local additions

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class JavaHelloWorldTest {
 
     @Test
-    void getHello() {
+    void getHello(){
         System.out.println(new JavaHelloWorld().getHello());
 
     }
